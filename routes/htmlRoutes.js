5678ac30fd160bae95f20fad6f27f1e56145998a
Mendezki <43857047+Mendezki@users.noreply.github.com>
2019-03-03 08:18:21 -0500

    Add files via upload

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,59 @@
 var db = require("../models");
+var path = require("path");
+
 
 module.exports = function(app) {
-  // Load index page
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    res.render("index", { 
+      title: "Balue", 
+      condition: false
     });
   });
+  
+  app.get("/search", function (req, res) {
+    res.render("search", { 
+      title: "Balue finds"
+    });
+    
+    //   db.Deals.findAll({
+    //   }).then(function (deals) {
+    //     res.render("search", {
+    //       results: results
+    //     });
+    //   });
+    });
+
+  app.get("/submit", function(req, res) {
+    res.render("submit", { title: "Submit your deal!"});
+  });
+
+
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../views/index.html"));
+  // });
+
+  // app.get("/search", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../views/search.html"));
+
+    // db.Example.findAll({}).then(function (dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+  // });
+
+  /////////////////////////////////////////////////
+  // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
