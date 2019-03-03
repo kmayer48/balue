5efb6704d6373550048dceb65ee5d43ae2d8b919
Mendezki <43857047+Mendezki@users.noreply.github.com>
2019-03-03 09:41:47 -0500

    Changes to htmlRoutes
    now hbs files are working with this, still working on getting response to populate the cards

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,60 @@
 var db = require("../models");
 var path = require("path");
 
+
 module.exports = function(app) {
-  
+
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
-  });
-
-  app.get("/views/index.html", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
-  });
-  
-  app.get("/views/search.html", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/search.html"));
-  });
-
-  app.get("/views/submit.html", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/submit.html"));
-  });
-
-  app.get("/views/submit.html", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/submit.html"));
-  });
-  /////////////////////////////////////////////////
-  // Load index page
-  app.get("/views/search.html", function(req, res) {
-    db.deals.findAll({}).then(function(dbDeals) {
-      res.render("/views/search.html", dbDeals);
+    res.render("index", { 
+      title: "Balue", 
+      condition: false
     });
   });
+  
+  app.get("/search", function (req, res) {
+    res.render("search", { 
+      title: "Balue finds"
+    });
+    
+      db.deals.findAll().then(function (deals) {
+        res.render("search", {
+          results: results
+        });
+        console.log(results);
+        console.log(deals);
+      });
+    });
+
+  app.get("/submit", function(req, res) {
+    res.render("submit", { title: "Submit your deal!"});
+  });
+
+
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../views/index.html"));
+  // });
+
+  // app.get("/search", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../views/search.html"));
+
+    // db.Example.findAll({}).then(function (dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+  // });
+
+  /////////////////////////////////////////////////
+  // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
